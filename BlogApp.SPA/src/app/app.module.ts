75d6e59f2f1afe90b9c7d7a54dbf5eb6ed7c4954
Lam Son <sonlam1807@gmail.com>
2018-11-04 23:07:42 +0700

    add SPA

---
+++ BlogApp.SPA/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { ListsComponent } from './lists/lists.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { appRoutes } from './routes';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      NavComponent,
+      RegisterComponent,
+      HomeComponent,
+      ListsComponent,
+      MemberListComponent,
+      MessagesComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      FormsModule,
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
+   ],
+   providers: [
+      AuthService,
+      ErrorInterceptorProvider,
+      AlertifyService,
+      AuthGuard
+   ],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule {}
